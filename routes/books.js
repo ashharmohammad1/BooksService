5f6057ffd89f37b5cd1daf72cda11c859c071f65
ashharmohammad1 <ashharmohammad1@gmail.com>
2022-11-07 11:19:18 +0530

    reading json file

--- routes/books.js
+++ routes/books.js
@@ -1,35 +1,56 @@
 const express = require('express')
 const router = express.Router()
 
-var books = [{
-  "Title": "Lord of the Rings",
-  "Author": "J.R.R Tolkien",
-  "price": 25.99,
-  "ISBN": "9780261102385",
-  "publisher": "HarperCollins"
-},
-{
-  "Title": "The Hobbit",
-  "Author": "J.R.R Tolkien",
-  "price": 9.88,
-  "ISBN": "0261102214",
-  "publisher": "HarperCollins"
-},
-{
-  "Title": "Lord of Souls",
-  "Author": "Greg Keyes",
-  "price": 12.98,
-  "ISBN": "0345508025",
-  "publisher": "Del Rey"
-},
-{
-  "Title": "Chronicles of Narnia",
-  "Author": "C. S. Lewis",
-  "price": 41.77,
-  "ISBN": "0064471195",
-  "publisher": "HarperCollins"
+const fs = require('fs');
+
+
+
+let read_json_file = () => {
+    let file = './Booksjson.json';
+    return fs.readFileSync(file);
 }
-]
+
+
+
+exports.list = () => {
+    return JSON.parse(read_json_file());
+};
+
+
+
+
+
+let books = JSON.parse(read_json_file());
+
+// var books = [{
+//   "Title": "Lord of the Rings",
+//   "Author": "J.R.R Tolkien",
+//   "price": 25.99,
+//   "ISBN": "9780261102385",
+//   "publisher": "HarperCollins"
+// },
+// {
+//   "Title": "The Hobbit",
+//   "Author": "J.R.R Tolkien",
+//   "price": 9.88,
+//   "ISBN": "0261102214",
+//   "publisher": "HarperCollins"
+// },
+// {
+//   "Title": "Lord of Souls",
+//   "Author": "Greg Keyes",
+//   "price": 12.98,
+//   "ISBN": "0345508025",
+//   "publisher": "Del Rey"
+// },
+// {
+//   "Title": "Chronicles of Narnia",
+//   "Author": "C. S. Lewis",
+//   "price": 41.77,
+//   "ISBN": "0064471195",
+//   "publisher": "HarperCollins"
+// }
+// ]
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
