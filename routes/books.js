edfb80b372aeb6ba1508ab7fd602ee0fb9919928
ashharmohammad1 <ashharmohammad1@gmail.com>
2022-11-09 09:26:39 +0530

    team name change

--- routes/books.js
+++ routes/books.js
@@ -20,37 +20,37 @@ exports.list = () => {
 
 
 
-let books = JSON.parse(read_json_file());
+// let books = JSON.parse(read_json_file());
 
-// var books = [{
-//   "Title": "Lord of the Rings",
-//   "Author": "J.R.R Tolkien",
-//   "price": 25.99,
-//   "ISBN": "9780261102385",
-//   "publisher": "HarperCollins"
-// },
-// {
-//   "Title": "The Hobbit",
-//   "Author": "J.R.R Tolkien",
-//   "price": 9.88,
-//   "ISBN": "0261102214",
-//   "publisher": "HarperCollins"
-// },
-// {
-//   "Title": "Lord of Souls",
-//   "Author": "Greg Keyes",
-//   "price": 12.98,
-//   "ISBN": "0345508025",
-//   "publisher": "Del Rey"
-// },
-// {
-//   "Title": "Chronicles of Narnia",
-//   "Author": "C. S. Lewis",
-//   "price": 41.77,
-//   "ISBN": "0064471195",
-//   "publisher": "HarperCollins"
-// }
-// ]
+var books = [{
+  "Title": "Lord of the Rings",
+  "Author": "J.R.R Tolkien",
+  "price": 25.99,
+  "ISBN": "9780261102385",
+  "publisher": "HarperCollins"
+},
+{
+  "Title": "The Hobbit",
+  "Author": "J.R.R Tolkien",
+  "price": 9.88,
+  "ISBN": "0261102214",
+  "publisher": "HarperCollins"
+},
+{
+  "Title": "Lord of Souls",
+  "Author": "Greg Keyes",
+  "price": 12.98,
+  "ISBN": "0345508025",
+  "publisher": "Del Rey"
+},
+{
+  "Title": "Chronicles of Narnia",
+  "Author": "C. S. Lewis",
+  "price": 41.77,
+  "ISBN": "0064471195",
+  "publisher": "HarperCollins"
+}
+]
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
@@ -59,9 +59,8 @@ router.use((req, res, next) => {
 })
 
 router.get('/team', (req, res) => {
-  res.json({ "member 1": "C B Layamitra",
-              "member 2": "Kritika M Pai",
-            "member 2":"Mohammad Ashhar Arif"  })
+  res.json({ "team": "Team books Backend",
+              "membersName":["CB Layamitra","Krithika M Pai","Mohammad Ashhar Arif"] })
 })
 
 // define the home page route
@@ -69,22 +68,25 @@ router.get('/', (req, res) => {
   res.json({ "hello": "there" })
 })
 // define the about route
-router.get('/all/:location', (req, res) => {
+router.get('/all/:location?', (req, res) => {
 
   location = req.params.location
+  if(location){
   if (location == "raleigh")
     for (item in books) {
-      item.price += 0.075 * item.price
+      console.log(item)
+      books[item].price+= 0.075 * books[item].price
+      // item.price += 0.075 * item.price
     }
   else if (location == "durham")
     for (item in books) {
-      item.price += 0.08 * item.price
+      books[item].price += 0.08 * books[item].price
     }
   else {
     res.send("wrong data")
     return
   }
-
+  }
 
   res.send(books)
 })
